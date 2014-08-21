6bf18780960c90e7f95fd17cea1285968e2a4556
Zhihao Hao <zhao@Zhihaos-MacBook-Pro.local>
2014-08-21 15:37:37 -0400

    Initial Commit

---
+++ vmc_spin_ice/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  vmc_spin_ice
+//
+//  Created by Zhihao Hao on 2014-08-21.
+//  Copyright (c) 2014 Zhihao Hao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
